157f5f12976dd3e7c00304fbe10a48d943ab659f
ssarioglu <serdar.sarioglu@mysystem.org>
2018-12-29 14:59:27 +0300

    ok

--- scripts/web-check.sh
+++ scripts/web-check.sh
@@ -12,10 +12,11 @@ for (( ; ; )); do
     mv new.html old.html 2> /dev/null
     curl $URL -L --compressed -s > new.html
     DIFF_OUTPUT="$(diff new.html old.html)"
+    clear
     if [ "0" != "${#DIFF_OUTPUT}" ]; then
         echo $(date +"%T") "$1 changed!"
     else
-        echo $(date +"%T") "No Change Detected"
+        echo $(date +"%T") "No Change Detected Sleeping 10 Seconds"
     fi
     sleep 10
 done
